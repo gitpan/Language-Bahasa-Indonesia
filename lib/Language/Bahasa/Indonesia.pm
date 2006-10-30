f4133a5b46e4d13e68ca19d574dba3a08d10d646
Daniel Sirait <dns@cpan.org>
2006-10-30 05:43:28 +0000

    Import of DNS/Language-Bahasa-Indonesia-0.01 from CPAN.
    gitpan-cpan-distribution: Language-Bahasa-Indonesia
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DNS/Language-Bahasa-Indonesia-0.01.tar.gz
    gitpan-cpan-author:       DNS
    gitpan-cpan-maturity:     released

---
+++ lib/Language/Bahasa/Indonesia.pm
@@ -0,0 +1,86 @@
+
+
+package Language::Bahasa::Indonesia;
+
+use Filter::Util::Call;
+#use Filter::Simple;
+
+
+# Work in progress, please be patient ..
+
+
+
+
+
+
+
+
+
+1;
+
+
+
+__END__
+
+
+=head1 NAMA
+		Language::Bahasa::Indonesia - Write Perl program in Bahasa Indonesia
+
+=head1 SYNOPSIS
+		use Language::Bahasa::Indonesia qw(pemula);
+		cetak "Apa kabar";		# print "Apa kabar";
+		die "proses dihentikan";	# mati;
+		
+		# or
+		
+		use Language::Bahasa::Indonesia qw(pujangga);
+		buka EMAIL di "| /usr/lib/sendmail -t" atau mati "tidak bisa buka file\n".	# print "Apa kabar";
+		
+		bunuh aplikasi_sekarang.	# kill ('SIGKILL', $$);
+		
+
+=head1 DESKRIPSI
+		This module is more related to Lingua::Romana::Perligata but the writing
+		system is totaly different.
+		
+		`pemula` is just to simplify the language from English-based to Indonesian.
+		
+		`pujangga` is the another variation on a theme, and completely recast the
+		language and it's more like look similar to Perligata.pm
+
+=head1 AUTHOR
+		Daniel Sirait <dns@cpan.org>
+
+=head1 THANKS TO
+		Paul Marquess, Damian Conway and other people who helped this project
+		much more easier.
+		
+=head1 LICENSE
+		Choose one of the following : Perl Artistic License or GPL.
+
+=cut
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/Language/Bahasa/test.pl
@@ -0,0 +1,34 @@
+#!perl
+
+
+use Indonesia;
+
+
+
+
+cetak "cetak";
+
+
+
+
+
+__END__
+cetak "Halo ! Apa kabar dunia ??\n";
+
+rutin gokil{
+
+bunuh "$0 dengan PROCESS ID: $$ telah dibunuh\n";
+}
+
+
+=head1 DESCRIPTION
+		
+		Test program ini dibuat oleh DaNiels.
+		Filter::Util::Call dan Language::Bahasa::Indonesia akan di-porting ke C untuk
+		pengembangan lebih lanjut.
+
+=head1 AUTHOR
+		Daniels <dns@cpan.org>
+
+=head1 SEE ALSO
+		Filter::Util::Call
